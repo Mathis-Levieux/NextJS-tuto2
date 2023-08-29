3629ef14504bcc2360f088d2350ead662fc0f785
Mathis <mathis.levieux1@gmail.com>
2023-08-30 01:22:04 +0200

    git init

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+    title: "About Traversy Media",
+}
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Traversy Media</h1>
+            <p>
+                lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Quisquam quod, voluptatum, voluptatem,
+                quibusdam quia quae voluptate quos dolorum
+            </p>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ app/api/courses/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import courses from './data.json';
+
+export async function GET(request) {
+    return NextResponse.json(courses);
+}
+
+export async function POST(request) {
+    const { title, description, level, link } = await request.json()
+    return NextResponse.json(course)
+}
